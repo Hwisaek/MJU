7edbf2badb4c732c402c10c9ca557447979d3af1
LeeChangMin <dia_changmin@naver.com>
2020-05-27 20:35:27 +0900

    초기업로드

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+    $number = $_GET['number'];
+    echo $number;
+    $connect = mysqli_connect("localhost", "root", "", "facerecog");
+    $query = "delete from facerecog.board where number =$number";
+    $result = $connect->query($query);
+?>
+
+<script>
+location.replace("./index.php");
+</script>
---
+++ upload_test.php
@@ -0,0 +1,17 @@
+<?php
+
+        	$id=$_POST[id];
+echo "id:".$id;
+$jpg=$id.'.jpg';
+	$python = `python capture.py`;
+	$filename=rename("recognizeface.jpg","F:/web/Apache24/htdocs/knowns/".$jpg);
+
+if($filename){
+        echo "<br>jpgname:".$id."<br>파일이름 변경 성공";
+    }else{
+        echo "<파일이름 변경 실패"; 
+    }
+?>
+<script>
+location.replace("./login.php");
+</script>
