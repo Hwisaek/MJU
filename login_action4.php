385f8ec6a77fe3e346a59debc5dd2164efa4efa8
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2020-06-04 17:09:55 +0900

    fix

--- login_action4.php
+++ login_action4.php
@@ -1,25 +1,21 @@
 <?php
-    session_start();
-    $python = `python eye.py`;
-    echo $python;
+  session_start();
+  $python = `python eye.py`;
+  echo $python;
 
-    if($python==($_SESSION['eye'])) {
-    ?>      <script>
-
-                alert("최종 로그인 성공");
-                location.replace("./index.php");
-            </script>
-<?php
-        }
-     else{
-         ?><script>
-
-                alert("로그인 실패");
-                location.replace("./login.php");
-           </script>
-<?php
-
-     }
-
-
+  if(strpos($python,$_SESSION['eye'])==true) {
+    ?>
+    <script>
+      alert("최종 로그인 성공");
+      location.replace("./index.php");
+    </script>
+    <?php
+  }else{
+    ?>
+    <script>
+      alert("로그인 실패");
+      location.replace("./login.php");
+    </script>
+    <?php
+  }
 ?>
