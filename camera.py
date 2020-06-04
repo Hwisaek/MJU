8b902f75e636c205c7399c803f5f595fd4942f02
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2020-06-04 20:38:10 +0900

    주석

--- camera.py
+++ camera.py
@@ -5,13 +5,10 @@ import cv2
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
         self.video = cv2.VideoCapture(0)
         # 내장 카메라 외장카메라에서 영상을 받아옴 0은 노트북 내장카메라
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
+        # 여러대의 카메라가 연결되어 있을 경우 0 대신 1, 2 등 숫자를 높여가며 확인할 것
+        # 카메라로 촬영이 아니라 영상을 이용하는 경우 아래의 코드 사용
         # self.video = cv2.VideoCapture('video.mp4')
 
     def __del__(self):
@@ -19,24 +16,39 @@ class VideoCamera(object):
         # 카메라 장치에서 받아온 메모리 해제
 
     def get_frame(self):
-        # Grab a single frame of video
+        # 영상으로부터 프레임을 가져옴
+        # 2개의 값을 리턴하는데 ret는 프레임을 성공적으로 읽었는지 반환
+        # frame 은 프레임 자체를 반환
         ret, frame = self.video.read()
         return frame
 
 
 if __name__ == '__main__':
+    # cam 객체 생성
     cam = VideoCamera()
     while True:
+
+        # cam 으로부터 frame을 가져옴
         frame = cam.get_frame()
 
-        # show the frame
+        # 받아온 frame을 화면에 표시
         cv2.imshow("Frame", frame)
+
+        # 지정된 시간동안 키보드 입력을 기다림. 1ms 단위
+        # cv.waitKey(0)은 키보드 입력이 있을 때까지 계속 대기.
+        # 64비트는 '& 0xFF' 연산을 해야함. 32비트의 운영체제는 생략가능
         key = cv2.waitKey(1) & 0xFF
 
-        # if the `q` key was pressed, break from the loop
+        # q가 입력 되면 while 문 종료
+        # ord() 함수는 문자를 아스키 코드 값으로 변환하는 함수
+        # chr() 함수는 아스키 코드 값을 문자로 변환하는 함수
+        # ord('c') 는 99를 리턴
+        # chr(99) 는 'c' 를 리턴
         if key == ord("q"):
             break
 
-    # do a bit of cleanup
+    # 생성한 모든 윈도우 제거
     cv2.destroyAllWindows()
+
+
     print('finish')
--- capture.py
+++ capture.py
@@ -1,4 +1,4 @@
-# camera.py
+# capture.py
 
 import cv2
 import os
@@ -6,32 +6,34 @@ import os
 
 class VideoCamera(object):
     def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
         self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
+        # 내장 카메라 외장카메라에서 영상을 받아옴 0은 노트북 내장카메라
+        # 여러대의 카메라가 연결되어 있을 경우 0 대신 1, 2 등 숫자를 높여가며 확인할 것
+        # 카메라로 촬영이 아니라 영상을 이용하는 경우 아래의 코드 사용
         # self.video = cv2.VideoCapture('video.mp4')
 
     def __del__(self):
         self.video.release()
+        # 카메라 장치에서 받아온 메모리 해제
 
     def get_frame(self):
-        # Grab a single frame of video
+        # 영상으로부터 프레임을 가져옴
+        # 2개의 값을 리턴하는데 ret는 프레임을 성공적으로 읽었는지 반환
+        # frame 은 프레임 자체를 반환
         ret, frame = self.video.read()
         return frame
 
 if __name__ == '__main__':
     cam = VideoCamera()
     path = os.path.dirname(__file__)
-    filename = "recognizeface.jpg"
+    filename = "recognizeface.jpg" # 캡쳐한 화면을 recognizeface.jpg 로 저장
     fullpath = os.path.join(path, filename)
+
     i = 0
     while True:
         frame = cam.get_frame()
         cv2.imshow("Frame", frame)
-        if i == 3:
+        if i == 3: # DroidCam 등으로 연결된 카메라의 경우 로딩시간으로 인해 3으로 설정해야 촬영 가능
             cv2.imwrite(fullpath, frame)
             break
         i += 1
