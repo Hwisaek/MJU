d10e949758553f0174a62e8dd72f8936b25631c6
Hwisaek <50049254+Hwisaek@users.noreply.github.com>
2020-06-20 21:12:33 +0900

    Save: 중간저장 hgrant 작동, hreq 제작시작

--- hw8/hw8.srcs/sources_1/new/myCPU.v
+++ hw8/hw8.srcs/sources_1/new/myCPU.v
@@ -22,7 +22,10 @@ reg hreq_r;
 assign hwdata = hwdata_r,
         haddr = haddr_r,
         hreq = hreq_r;
-        
+// hreq 초기화        
+initial
+    hreq_r = 0;
+    
 // 상태기
  always @ (state)
     case (state)
@@ -46,13 +49,13 @@ always @ (state, hrdata)
         if (state == S0)
             case (opcode) 
                 // ADD, SUB, AND, OR 일 때는 피연산자를 3개로 나눔 
-                ADD: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; end
-                SUB: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; end
-                AND: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; end
-                OR: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; end
+                ADD: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; hreq_r = 0; end
+                SUB: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; hreq_r = 0; end
+                AND: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; hreq_r = 0; end
+                OR: begin oper1 = hrdata[4:7]; oper2 = hrdata[8:11]; oper3 = hrdata[12:15]; hreq_r = 0; end
                 // LD, ST 는 oper1에 레지스터 주소를 저장, 뒷부분에는 메모리 주소를 저장
-                LD: begin oper1 = hrdata[4:7]; mem_addr = hrdata[8:15]; end
-                ST: begin oper1 = hrdata[4:7]; mem_addr = hrdata[8:15];  end
+                LD: begin oper1 = hrdata[4:7]; mem_addr = hrdata[8:15];  hreq_r = 1;end
+                ST: begin oper1 = hrdata[4:7]; mem_addr = hrdata[8:15];  hreq_r = 1;end
             endcase
         else if(state == S1) // 상태가 S1 인 경우 
         begin
@@ -115,7 +118,7 @@ end
 // 레지스터 값 초기화
 initial
 begin
-for (i = 0; i < 16; i = i + 1)
+    for (i = 0; i < 16; i = i + 1)
         R_in[i] <= i;
 end
 
@@ -134,6 +137,7 @@ wire [0:15] data;
 wire [0:7] addr;
 assign addr = haddr;
 assign data = hwdata;
+assign hrdata = hrdata_r;
 
 reg state, next_state;
 parameter S0 = 0, S1 = 1;// S0 = 주소가 255가 아닌 모든 경우  , S1 = ST 인 경우/주소로 255가 들어올 때  
@@ -153,6 +157,7 @@ always @ (posedge clk)
     else
         state <= next_state;
 
+// 메모리 초기화
 initial
 begin
     for ( count = 0; count < 100; count = count + 1)
@@ -180,29 +185,31 @@ begin
         end
 end
     
-
-always @ (posedge clk)
+// 상태에 따른 case
+always @ (*)
     if (state == S0)
         hrdata_r = M[haddr];
     else if (state == S1)    
         M[addr] = data;
         
-assign hrdata = hrdata_r;
 endmodule
 
 
 // 아비터 모듈 -----------------------------------------------------------------------------------------------------------------
-module arbiter(hgrant, hreq1, hreq2);
+module arbiter(hgrant, hreq1, hreq2, clk, reset_n);
 output hgrant;
-input hreq1, hreq2;
+input hreq1, hreq2, reset_n, clk;
 
 reg hgrant_r;
 
-always @ (hreq1, hreq2)
-    if(hreq1)
-        hgrant_r = 1;
+always @ (clk)
+    if(~reset_n)
+        hgrant_r <= 1;
     else
-        hgrant_r = 0;
+        if(hreq1)
+            hgrant_r <= 1;
+        else
+            hgrant_r <= 0;
     
 assign hgrant = hgrant_r;
 endmodule
@@ -216,34 +223,45 @@ wire hreq1, hreq2, hgrant;
 
 reg [0:7] haddr;
 reg [0:15] hwdata;
-reg clk, reset_n, hgrant_n;
+reg clk, reset_n, hgrant_n, hgrant_r;
+
+assign hgrant = hgrant_r;
 
 myCPU CPU1(haddr1, hwdata1, hreq1, hgrant, hrdata1, clk, reset_n);
 myCPU CPU2(haddr2, hwdata2, hreq2, hgrant_n, hrdata2, clk, reset_n);
 myMem Mem(hrdata, haddr, hwdata, clk, reset_n);
-arbiter Arb(hgrant, hreq1, hreq2);
+// arbiter Arb(hgrant, hreq1, hreq2, clk, reset_n);
 
-always @ (hgrant) // hgrant = 1 -> CPU1 작동 , hgrant = 0 -> CPU2 작동 
+always @ (*) // hgrant = 1 -> CPU1 작동 , hgrant = 0 -> CPU2 작동 
 begin
     hgrant_n <= ~hgrant;
     haddr <= hgrant? haddr1 : haddr2;
     hwdata <= hgrant? hwdata1 : hwdata2;
     case(hgrant)
-        1: hrdata1 = hrdata;
-        0: hrdata2 = hrdata;
+        1:  begin
+                hrdata1 <= hrdata;
+                hrdata2 <= 16'bx;
+            end
+        0:  begin
+                hrdata2 <= hrdata;
+                hrdata1 <= 16'bx;
+            end
     endcase
 end
 
 initial
 begin
-    reset_n = 0;
-    #40 reset_n = 1;
+    hgrant_r <= 1;
+    reset_n <= 0;
+    reset_n = #40 1;
+    #1000 hgrant_r <= 0;
+    #1000 hgrant_r <= 1;
 end
 
 initial
 begin
-    clk = 1;
-    forever #10 clk = ~clk;
+    clk <= 1;
+    forever #10 clk <= ~clk;
 end
 
 initial
